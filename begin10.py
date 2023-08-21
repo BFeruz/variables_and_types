6ec5b4499f4f91063785a5dfba2c66c949a319c8
BFeruz <bahodirovferuz873@gmail.com>
2023-08-21 18:14:16 +0500

    init comment

--- begin10.py
+++ begin10.py
@@ -1,11 +1,6 @@
-# Assign 5 (int value) to 'a' variable;
-
-# Assign 4 (int value) to 'b' variable;
-
-# Assign 2 (int value) to 'c' variable;
-
-# Assign 8 (int value) to 'd' variable;
-
-# Find the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin10' to variable 'k';
-
-# # Print the variable 'k'.
+n=5428
+a=n//1000%10
+b=n//100%10
+c=n//10%10
+d=n%10
+print(2*(a*b-c*d))
