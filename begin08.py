72d133927e7197275bb5665d1cc7bf1fc1baaca4
BFeruz <bahodirovferuz873@gmail.com>
2023-08-21 18:02:57 +0500

    init comment

--- begin08.py
+++ begin08.py
@@ -1,7 +1,5 @@
-# Assign 5 (int value) to 'a' variable;
-
-# Assign 4 (int value) to 'b' variable;
-
-# Assign 8 (int value) to 'c' variable;
-
-# Print the result of that expression 'https://github.com/Final-PythonFoundationHomework/variables_and_types/blob/main/README.md#begin08';
+n=548
+a=n//100%10
+b=n//10%10
+c=n%10
+print(a*b/c)
